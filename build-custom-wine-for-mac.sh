f9e4f4a055cfc14f9bae220f0bdec1da2d334ac3
Peter Gill <peter.gill@townsuite.com>
2025-01-09 13:47:46 -0330

    Update build-custom-wine-for-mac.sh

--- build-custom-wine-for-mac.sh
+++ build-custom-wine-for-mac.sh
@@ -11,58 +11,39 @@ install_deps(){
     # install build dependencies
     xcode-select --install
     brew install --formula bison mingw-w64 pkgconfig
+    brew install llvm lld
+    brew install --cask gstreamer-development
+    echo 'export PATH="/opt/homebrew/opt/bison/bin:$PATH"' >> ~/.zshrc
+    echo 'export PATH="/opt/homebrew/opt/llvm/bin:$PATH"' >> ~/.zshrc
+    echo 'export PATH="/opt/homebrew/Cellar/lld/19.1.6/bin:$PATH"' >> ~/.zshrc
+    source ~/.zshrc    
 
     # install runtime dependencies
     brew install —-formula freetype gnutls molten-vk sdl2
 }
 
-build_wine(){
-
+build_wine_x86_64(){
+    export CPPFLAGS="-I/opt/homebrew/include -I/opt/homebrew/Cellar/molten-vk/1.2.11/include"
+    export LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/Cellar/molten-vk/1.2.11/lib -L/opt/homebrew/opt/bison/lib"
+    # /opt/homebrew/Cellar/vulkan-headers/1.4.304
+    
     WINE_BRANCH=wine-9.21
 
     mkdir -p $CWD/build
 
-    if [ ! -d "$CWD/build/wine-tools/.git" ]; then
-        git clone https://gitlab.winehq.org/wine/wine.git $CWD/build/wine-tools
+    if [ ! -d "$CWD/build/wine-x86_64/.git" ]; then
+        git clone https://gitlab.winehq.org/wine/wine.git $CWD/build/wine-x86_64
     fi
-    cd $CWD/build/wine-tools
-    git config --global --add safe.directory $CWD/build/wine-tools
+    cd $CWD/build/wine-x86_64
+    git config --global --add safe.directory $CWD/build/wine-x86_64
     git fetch origin
      if [ "$(git rev-parse --abbrev-ref HEAD)" != "branch-$WINE_BRANCH" ]; then
         git checkout --no-track -b branch-$WINE_BRANCH $WINE_BRANCH --
     fi
     
-    
-    if [ ! -d "$CWD/build/wine/.git" ]; then
-        git clone https://gitlab.winehq.org/wine/wine.git $CWD/build/wine
-    fi
-    cd $CWD/build/wine
-    git config --global --add safe.directory $CWD/build/wine
-    git fetch origin
-    if [ "$(git rev-parse --abbrev-ref HEAD)" != "branch-$WINE_BRANCH" ]; then
-        git checkout --no-track -b branch-$WINE_BRANCH $WINE_BRANCH --
-    fi
-    
-
-    cd $CWD/build/wine-tools 
-    #./configure --without-x 
-    #make __tooldeps__
-
-    cd $CWD/build/wine
-
-    # Set up environment variables for cross-compilation
-    # windows x64
-    #export CC=x86_64-w64-mingw32-gcc
-    #export CXX=x86_64-w64-mingw32-g++
-    #export PATH=/usr/x86_64-w64-mingw32/bin:$PATH
-
-    
-    export WINE_TOOLS="$CWD/build/wine-tools"
-
-    #./configure --host=x86_64-apple-darwin --with-wine-tools=$WINE_TOOLS --enable-win64 \
-    ./configure --disable-option-checking \
+    cd $CWD/build/wine-x86_64
+     ./configure --host=x86_64-apple-darwin --enable-win64 --disable-option-checking \
     --disable-tests \
-    --enable-archs=i386,x86_64 \
     --without-alsa \
     --without-capi \
     --with-coreaudio \
@@ -75,8 +56,8 @@ build_wine(){
     --without-gphoto \
     --with-gnutls \
     --without-gssapi \
-    --with-gstreamer \
-    --with-inotify \
+    --without-gstreamer \
+    --without-inotify \
     --without-krb5 \
     --with-mingw \
     --without-netapi \
@@ -88,7 +69,81 @@ build_wine(){
     --with-pthread \
     --without-pulse \
     --without-sane \
-    --with-sdl \
+    --without-sdl \
+    --without-udev \
+    --with-unwind \
+    --without-usb \
+    --without-v4l2 \
+    --with-vulkan \
+    --without-wayland \
+    --without-x
+
+    # build wine
+    make -j$(nproc)
+
+    make install DESTDIR=$CWD/build/wine-build
+    cd $CWD/build/wine-build
+    tar -czvf $CWD/build/wine-macos-x86_64.tar.gz .
+}
+
+
+build_wine_aarch64(){
+    export CPPFLAGS="-I/opt/homebrew/include -I/opt/homebrew/Cellar/molten-vk/1.2.11/include"
+    export LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/Cellar/molten-vk/1.2.11/lib -L/opt/homebrew/opt/bison/lib"
+    # /opt/homebrew/Cellar/vulkan-headers/1.4.304
+    
+    WINE_BRANCH=wine-9.21
+
+    mkdir -p $CWD/build
+
+    if [ ! -d "$CWD/build/wine-aarch64/.git" ]; then
+        git clone https://gitlab.winehq.org/wine/wine.git $CWD/build/wine-aarch64
+    fi
+    cd $CWD/build/wine-aarch64
+    git config --global --add safe.directory $CWD/build/wine-aarch64
+    git fetch origin
+    if [ "$(git rev-parse --abbrev-ref HEAD)" != "branch-$WINE_BRANCH" ]; then
+        git checkout --no-track -b branch-$WINE_BRANCH $WINE_BRANCH --
+    fi
+    
+   
+    cd $CWD/build/wine-aarch64
+
+    # Set up environment variables for cross-compilation
+    # windows x64
+    #export CC=x86_64-w64-mingw32-gcc
+    #export CXX=x86_64-w64-mingw32-g++
+    #export PATH=/usr/x86_64-w64-mingw32/bin:$PATH
+
+
+    ./configure --host=aarch64-apple-darwin --enable-win64 --disable-option-checking \
+    --disable-tests \
+    --without-alsa \
+    --without-capi \
+    --with-coreaudio \
+    --with-cups \
+    --without-dbus \
+    --without-fontconfig \
+    --with-freetype \
+    --with-gettext \
+    --without-gettextpo \
+    --without-gphoto \
+    --with-gnutls \
+    --without-gssapi \
+    --without-gstreamer \
+    --without-inotify \
+    --without-krb5 \
+    --with-mingw \
+    --without-netapi \
+    --with-opencl \
+    --with-opengl \
+    --without-oss \
+    --with-pcap \
+    --with-pcsclite \
+    --with-pthread \
+    --without-pulse \
+    --without-sane \
+    --without-sdl \
     --without-udev \
     --with-unwind \
     --without-usb \
@@ -104,6 +159,18 @@ build_wine(){
     cd $CWD/build/wine-build
     tar -czvf $CWD/build/wine-macos64.tar.gz .
 
+    lipo -create -output $CWD/build/wine-output/wine64 $CWD/build/wine-x86_64/wine64 $CWD/build/wine-aarch/wine64
+    mkdir -p $CWD/build/wine-universal
+    lipo -info $CWD/build/wine-universal/wine64
 }
 
-build_wine
+
+function create_universal_package(){
+    lipo -create -output $CWD/build/wine-output/wine64 $CWD/build/wine-x86_64/wine64 $CWD/build/wine-aarch/wine64
+    mkdir -p $CWD/build/wine-universal
+    lipo -info $CWD/build/wine-universal/wine64
+}
+
+#build_wine_x86_64
+build_wine_aarch64
+#create_universal_package
